b373a8907dd3d660765f3fb7ffbffd1622d63002
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-18 16:16:54 +0530

    creating the MVC design pattern and calling the user information through the JWT token and if use is valid then send the result else access denied status show

---
+++ HISTORICAL-VENUE-OF-MAHARASHTRA/src/main/java/com/project/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+
+package com.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.project.JwtTokenCreate.JwtAuthenticationEntryPoint;
+import com.project.JwtTokenCreate.JwtAuthenticationFilter;
+import com.project.services.CustomUserDetailService;
+import com.project.services.UserService;
+
+import jakarta.annotation.security.PermitAll;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private JwtAuthenticationEntryPoint point;
+
+	@Autowired
+	private JwtAuthenticationFilter filter;
+
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserService userService;
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().disable()
+				.authorizeRequests(auth -> auth.requestMatchers("/auth/login/**", "/auth/create-user/**").permitAll()
+						.requestMatchers("/test/users/**").hasAuthority("ADMIN")// Only users with role "ADMIN" can
+																				// access this endpoint
+						.anyRequest().authenticated())
+				.userDetailsService(customUserDetailService).exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+				// change in code
+				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(customUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
+
+}
